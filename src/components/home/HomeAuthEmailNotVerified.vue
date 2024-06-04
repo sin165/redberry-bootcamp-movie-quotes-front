f36543a9a226d211908af1ab54ab32012f3eb05c
Tamar Sinauridze <sin165165@gmail.com>
2024-06-04 23:29:53 +0400

    feat: add email verification

--- src/components/home/HomeAuthEmailNotVerified.vue
+++ src/components/home/HomeAuthEmailNotVerified.vue
@@ -2,23 +2,39 @@
 import LayoutsAuthNotice from '@/layouts/LayoutsAuthNotice.vue'
 import BaseButton from '@/components/base/BaseButton.vue'
 import IconWarning from '@/components/icons/IconWarning.vue'
+import { useFetch } from '@/composables/useFetch'
+
+const emit = defineEmits(['sent'])
 
 const props = defineProps<{
   email: string
+  expired?: boolean
 }>()
 
-const resendEmail = () => {
-  console.log(props.email)
+const { status, loading, formValues, executeFetch } = useFetch(
+  '/email/verification-notification',
+  'POST',
+  true
+)
+formValues.value.email = props.email
+
+const resendEmail = async () => {
+  await executeFetch()
+  if (status.value === 200) emit('sent')
 }
 </script>
 
 <template>
   <LayoutsAuthNotice>
     <template v-slot:icon><IconWarning /></template>
-    <template v-slot:title>Account not active!</template>
-    <template v-slot:text>You need to verify your email before you can log in.</template>
+    <template v-slot:title>{{ expired ? 'Link expired!' : 'Account not active!' }}</template>
+    <template v-slot:text>{{
+      expired
+        ? 'Login link has expired, because you haven’t used it'
+        : 'You need to verify your email before you can log in.'
+    }}</template>
     <template v-slot:button>
-      <BaseButton @click="resendEmail">Request another link</BaseButton>
+      <BaseButton :loading="loading" @click="resendEmail">Request another link</BaseButton>
     </template>
   </LayoutsAuthNotice>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,17 +4,29 @@ import HomeAuthRegister from '@/components/home/HomeAuthRegister.vue'
 import HomeAuthLogin from '@/components/home/HomeAuthLogin.vue'
 import HomeAuthEmailNotVerified from '@/components/home/HomeAuthEmailNotVerified.vue'
 import HomeAuthVerificationEmailSent from '@/components/home/HomeAuthVerificationEmailSent.vue'
+import HomeAuthAccountActivated from '@/components/home/HomeAuthAccountActivated.vue'
 import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { useFetch } from '@/composables/useFetch'
 
+const route = useRoute()
 const registerOpen = ref(false)
 const loginOpen = ref(false)
 const emailNotVerifiedOpen = ref(false)
 const verificationEmailSentOpen = ref(false)
+const accountActivatedOpen = ref(false)
+const linkExpired = ref(false)
 const emailAddress = ref('')
 
-const switchBetweenLoginAndRegister = () => {
-  loginOpen.value = !loginOpen.value
-  registerOpen.value = !registerOpen.value
+const switchToLogin = () => {
+  loginOpen.value = true
+  registerOpen.value = false
+  accountActivatedOpen.value = false
+}
+
+const switchToRegister = () => {
+  registerOpen.value = true
+  loginOpen.value = false
 }
 
 const handleEmailNotVerified = (email: string) => {
@@ -28,6 +40,27 @@ const handleRegistered = (email: string) => {
   emailAddress.value = email
   verificationEmailSentOpen.value = true
 }
+
+const handleSent = () => {
+  verificationEmailSentOpen.value = true
+  emailNotVerifiedOpen.value = false
+}
+
+const verifyEmail = async () => {
+  if (route.query.verify_url && route.query.email) {
+    const { status, executeFetch } = useFetch(route.query.verify_url.toString())
+    await executeFetch()
+    if (status.value === 200) {
+      accountActivatedOpen.value = true
+    } else {
+      emailAddress.value = route.query.email.toString()
+      linkExpired.value = true
+      emailNotVerifiedOpen.value = true
+    }
+  }
+}
+
+verifyEmail()
 </script>
 
 <template>
@@ -40,24 +73,31 @@ const handleRegistered = (email: string) => {
     <HomeAuthRegister
       v-if="registerOpen"
       @close="registerOpen = false"
-      @login-click="switchBetweenLoginAndRegister"
+      @login-click="switchToLogin"
       @registered="handleRegistered"
     />
     <HomeAuthLogin
       v-if="loginOpen"
       @close="loginOpen = false"
-      @register-click="switchBetweenLoginAndRegister"
+      @register-click="switchToRegister"
       @email-not-verified="handleEmailNotVerified"
     />
     <HomeAuthEmailNotVerified
       v-if="emailNotVerifiedOpen"
       :email="emailAddress"
+      :expired="linkExpired"
       @close="emailNotVerifiedOpen = false"
+      @sent="handleSent"
     />
     <HomeAuthVerificationEmailSent
       v-if="verificationEmailSentOpen"
       :email="emailAddress"
       @close="verificationEmailSentOpen = false"
+    />
+    <HomeAuthAccountActivated
+      v-if="accountActivatedOpen"
+      @close="accountActivatedOpen = false"
+      @login-click="switchToLogin"
     />
   </div>
 </template>
