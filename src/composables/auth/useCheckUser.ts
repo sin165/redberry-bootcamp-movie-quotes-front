338c6159c263b98ae2ccb9e42de6eaf6545d5a0a
Tamar Sinauridze <sin165165@gmail.com>
2024-06-06 23:52:39 +0400

    feat: fetch user when loading app

---
+++ src/composables/auth/useCheckUser.ts
@@ -0,0 +1,22 @@
+import { useFetch } from '@/composables/useFetch'
+import { useUserStore } from '@/store/UserStore'
+import { useRoute, useRouter } from 'vue-router'
+
+export function useCheckUser() {
+  const { data, status, executeFetch } = useFetch('/user')
+  const userStore = useUserStore()
+  const router = useRouter()
+  const route = useRoute()
+
+  const fetchUser = async () => {
+    await executeFetch()
+    if (status.value === 200) {
+      userStore.setUser(data.value.name, data.value.email)
+      if (route.name === 'home') {
+        router.push({ name: 'feed' })
+      }
+    }
+  }
+
+  fetchUser()
+}
