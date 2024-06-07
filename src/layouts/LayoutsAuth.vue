5bbd4f53f63d7d905b0ce0170a7383f1c21765b4
Tamar Sinauridze <sin165165@gmail.com>
2024-06-07 14:41:59 +0400

    feat: add a close button to auth modals

--- src/layouts/LayoutsAuth.vue
+++ src/layouts/LayoutsAuth.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import IconX from '@/components/icons/IconX.vue'
 const emit = defineEmits(['close'])
 </script>
 
@@ -9,6 +10,7 @@ const emit = defineEmits(['close'])
       class="bg-linear-dark-bottom lg:bg-night fixed top-0 left-1/2 -translate-x-1/2 w-107 max-w-full h-full lg:w-150 lg:top-52.5 lg:h-fit z-50 px-8.5 lg:px-30 lg:rounded-0.5xl lg:pb-13 font-neue"
     >
       <slot></slot>
+      <IconX class="absolute top-8 right-6 cursor-pointer lg:hidden" @click="emit('close')" />
     </div>
   </div>
 </template>
